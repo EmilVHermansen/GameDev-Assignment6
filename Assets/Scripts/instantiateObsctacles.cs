7e597f3586b9331fe137922a450ee64e3718cd1d
Anderskruse <32634165+anderskruse@users.noreply.github.com>
2019-03-25 15:52:58 +0100

    Infinite runner now works (green complete)(add png of obstacles)

---
+++ Assets/Scripts/instantiateObsctacles.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiateObsctacles : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject obstacleSquare;
+    public GameObject obstacleFly;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player, new Vector3(-11f, 0.2f, -10f), Quaternion.identity);
+        StartCoroutine(Wave());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Wave()
+    {
+        yield return new WaitForSeconds(1f);
+        for (int i = 0; i < 100; i++)
+        {
+
+            yield return new WaitForSeconds(2f);
+            int number = Random.Range(0, 2);
+            print(number);
+            if (number > 0)
+            {
+                Instantiate(obstacleSquare, new Vector2(11f, 0.2f), Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(obstacleFly, new Vector2(11f, 0.4f), Quaternion.identity);
+            }
+            yield return null;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/obstacle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacle : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        transform.Translate(-8f * Time.deltaTime, 0f, 0f);
+    }
+
+
+}
+
