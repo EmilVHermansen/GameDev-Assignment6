a403e388d05317fa213024daa2dc682947674170
anderskruse <amadsen1702@gmail.com>
2019-04-01 10:32:15 +0200

    Main menu working, android controls working. Need to fix infinite jump and point system.

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+    public Button playGame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playGame.onClick.AddListener(LoadScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("InfiniteRunner");
+    }
+}
--- Assets/Scripts/jump.cs
+++ Assets/Scripts/jump.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class jump : MonoBehaviour
 {
-
+    public float jumpForce = 275f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)
         {
+            Touch touch = Input.GetTouch(0);
 
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 250f));
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
         }
     }
 
@@ -20,9 +23,7 @@ public class jump : MonoBehaviour
     {
         if (other.gameObject.tag == "obstacle")
         {
-            Destroy(gameObject);
-            Instantiate(gameObject, new Vector3(-11f, 0.2f, -10f), Quaternion.identity);
-            gameObject.AddComponent<Rigidbody2D>();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
 
